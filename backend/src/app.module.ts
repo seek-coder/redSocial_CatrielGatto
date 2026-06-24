666d68a55c58e7dadfe92d6990eb30a184162e89
catriel <catrielgatto99@gmail.com>
2026-06-24 12:53:29 -0300

    feat: sprint 3, v5.1, .env completo

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,8 +6,11 @@ import { PublicacionesModule } from './publicaciones/publicaciones.module';
 import { ComentariosModule } from './comentarios/comentarios.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
 
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env['MONGODB_URI'] || 'mongodb+srv://gatto99:gatto99@prograiv.naiqon7.mongodb.net/red-social?retryWrites=true&w=majority&appName=PrograIV'),
     CloudinaryModule,
     AutenticacionModule,
